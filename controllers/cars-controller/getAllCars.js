63f7a45cd15ae53eb309f8ad80629f751d63b128
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2024-03-30 00:14:55 +0200

    add pagination

--- controllers/cars-controller/getAllCars.js
+++ controllers/cars-controller/getAllCars.js
@@ -1,7 +1,9 @@
 import Car from "../../models/Car.js";
 
 const getAllCars = async (req, res) => {
-  const allCars = await Car.find();
+  const { page = 1, limit = 12 } = req.query;
+  const skip = (page - 1) * limit;
+  const allCars = await Car.find({}, { skip, limit });
   res.json(allCars);
 };
 

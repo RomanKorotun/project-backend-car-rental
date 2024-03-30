25013c62b3f969cb41a06f6aedebc1abdfc58736
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2024-03-30 10:22:04 +0200

    update pagination

--- controllers/cars-controller/getAllCars.js
+++ controllers/cars-controller/getAllCars.js
@@ -1,9 +1,10 @@
 import Car from "../../models/Car.js";
 
 const getAllCars = async (req, res) => {
+  console.log(req.query);
   const { page = 1, limit = 12 } = req.query;
   const skip = (page - 1) * limit;
-  const allCars = await Car.find({}, { skip, limit });
+  const allCars = await Car.find({}, "", { skip, limit });
   res.json(allCars);
 };
 

efd20a9b5fe6d8b63786cd73e3b887e3c2c8cfed
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2024-04-07 16:10:28 +0300

    fix

--- middleware/isEmptyBody.js
+++ middleware/isEmptyBody.js
@@ -2,7 +2,7 @@ import { HttpError } from "../helpers/index.js";
 const isEmptyBody = (req, res, next) => {
   const length = Object.keys(req.body).length;
   if (length === 0) {
-    return HttpError(400, "missing fields");
+    return next(HttpError(400, "missing fields"));
   }
   next();
 };

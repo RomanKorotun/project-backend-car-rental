20b760c8f3dcdf27f72bb90f4d5acce37a685bb6
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2024-04-07 16:07:39 +0300

    fix

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -1,8 +1,9 @@
+import { HttpError } from "../helpers/index.js";
 const validateBody = (schema) => {
   const funct = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      return next(400, error.message);
+      return next(HttpError(400, error.message));
     }
     next();
   };
